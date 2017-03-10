abfac0401088be62ec4f63ea4e59fef811fce1d0
Stephan Tijink <stijink@googlemail.com>
2017-03-10 18:38:18 +0100

    Fixes

--- hue-raspberry-hdmi.js
+++ hue-raspberry-hdmi.js
@@ -1,11 +1,11 @@
 var exec   = require('child_process').exec;
 var HueApi = require("node-hue-api").HueApi;
 
-var enanleLogging = true;
+var intervalId = null;
 
 var HdmiOffCmd  = '/opt/vc/bin/tvservice -o';
 var HdmiOnCmd   = '/opt/vc/bin/tvservice -p && chvt 1 && chvt 7';
-var HdmiTimeout = 6000 * 5; // 5 Minutes
+var HdmiTimeout = 60000 * 5; // 5 Minutes
 var HdmiTimeoutId = null;
 
 var HueHostname = process.env.HUE_HOST;
@@ -27,23 +27,44 @@ var api = new HueApi(HueHostname, HueUsername);
 console.log('Checking for Light Id #' + HueLightId);
 console.log('HDMI Port will be turned off after ' + Math.floor(HdmiTimeout / 6000) + ' Minutes');
 
-// Check for the Status of the Light every second
-setInterval(turnHdmiOnIfLightIsOn, 1000);
+checkForLightEverySecond();
 
-function turnHdmiOnIfLightIsOn() {
-  console.log('Check if the Hue Light Id #' + HueLightId + ' is turned on ...');
-  api.lightStatus(HueLightId).then(turnHdmiOn).done();
+// Check for the Status of the Light every second
+function checkForLightEverySecond()
+{
+  intervalId = setInterval(turnHdmiOnIfLightIsOn, 1000);
 }
 
-function turnHdmiOff() {
+function turnHdmiOnIfLightIsOn()
+{
+  console.log('Check if the Hue Light Id #' + HueLightId + ' is turned on ...');
+  api.lightStatus(HueLightId).then(checkLightStatus).done();
+}
+
+function checkLightStatus(status)
+{
+  if (status.state.on == true) {
+    turnHdmiOn();
+  }
+}
+
+function turnHdmiOff()
+{
   console.log('HDMI Port turned off');
   exec(HdmiOffCmd, function(error, stdout, stderr) {});
+
+  // When the hdmi port is turned off we want to start
+  // checking for the light again.
+  checkForLightEverySecond();
 }
 
 function turnHdmiOn()
 {
   console.log('HDMI Port turned on');
   exec(HdmiOnCmd, function(error, stdout, stderr) {});
+
+  // Don't check for the light every second during it's on anyway
+  clearInterval(intervalId);
 
   // Turn off the HDMI Port after the time given in "HdmiTimeout"
   HdmiTimeoutId = setTimeout(turnHdmiOff, HdmiTimeout);
